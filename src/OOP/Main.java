3c4e84dc834d2e05db5d424b78c57696438c847c
Yunsik Kim <yunsik0115@uos.ac.kr>
2022-09-09 12:57:59 +0900

    Way to declare class and calling methods in it

---
+++ src/OOP/Main.java
@@ -0,0 +1,9 @@
+package OOP;
+
+public class Main {
+    public static void main(String[] args) {
+        Car porsche = new Car();
+        Car holden = new Car();
+        porsche.setModel("carrera");
+    }
+}
