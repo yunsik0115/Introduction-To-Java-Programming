c99b3fc8b63cc292f0dcdf6f71006d9d1a03924f
Yunsik Kim <yunsik0115@uos.ac.kr>
2022-09-09 13:12:55 +0900

    OOP-Challenge-1

---
+++ src/OOP_Challenge_1/Account.java
@@ -0,0 +1,82 @@
+package OOP_Challenge_1;
+
+public class Account {
+    // Create a new class for a bank account
+    // Create fields for the account number, balance, customer name, email and phone number.
+    //
+    // Create getters and setters for each field
+    // Create two additional methods
+    // 1. To allow the customer to deposit funds (this should increment the balance field).
+    // 2. To allow the customer to withdraw funds. This should deduct from the balance field,
+    // but not allow the withdrawal to complete if their are insufficient funds.
+    // You will want to create various code in the Main class (the one created by IntelliJ) to
+    // confirm your code is working.
+    // Add some System.out.println's in the two methods above as well.
+
+
+    public Account(String number, double balance, String customerName, String email, String phoneNumber) {
+        this.number = number;
+        this.balance = balance;
+        this.customerName = customerName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    private String number;
+    private double balance;
+    private String customerName;
+    private String email;
+    private String phoneNumber;
+
+    public void deposit(double money){
+        this.balance += money;
+    }
+
+    public void withdrawal(double money){
+        if(this.balance - money < 0){
+            System.out.println("Insufficient Balance, Cannot Withdraw money");
+        } else{
+            System.out.println("Withdraw Completed : Balance left " + (this.balance - money));
+        }
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
