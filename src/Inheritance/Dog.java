cda17384363818e9d478736652b0e968d5c6ae45
Yunsik Kim <yunsik0115@uos.ac.kr>
2022-09-10 12:05:31 +0900

    Inheritance Example Code Implementation + typo modified

--- src/Inheritance/Dog.java
+++ src/Inheritance/Dog.java
@@ -7,8 +7,14 @@ public class Dog extends Animal {
     private int tail;
     private int teeth;
     private String coat;
-    public Dog(String name, int brain, int body, int size, int weight) {
+
+    public Dog(String name, int brain, int body, int size, int weight, int eyes, int legs, int tail, int teeth, String coat) {
         super(name, brain, body, size, weight);
+        this.eyes = eyes;
+        this.legs = legs;
+        this.tail = tail;
+        this.teeth = teeth;
+        this.coat = coat;
     }
 
     @Override
