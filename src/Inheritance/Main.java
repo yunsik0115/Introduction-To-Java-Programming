80bb491e4d45db4bc510748bdda6e74681d27584
Yunsik Kim <yunsik0115@uos.ac.kr>
2022-09-10 12:06:36 +0900

    Inheritance Example Code Implementation + typo modified

--- src/Inheritance/Main.java
+++ src/Inheritance/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Animal animal = new Animal("Animal", 1, 1, 5, 5);
 
-        Dog dog = new Dog("Yorkie", 8, 20 ,2, 4 ,1 ,20, "long silky");
+        Dog dog = new Dog("Yorkie", 1, 20 ,2, 4 ,1 ,2, 1, 1,"long silky");
         dog.eat();
     }
 }
