497d977d9897f5a0d6d570428050243c6f6e4b07
Yunsik Kim <yunsik0115@uos.ac.kr>
2022-09-25 14:51:43 +0900

    Sorted Collections

---
+++ src/SortedCollection/StockList.java
@@ -0,0 +1,60 @@
+package SortedCollection;
+
+import java.util.Collections;
+import java.util.Map;
+
+public class StockList {
+    private final Map<String, StockItem> list;
+
+    public StockList(Map<String, StockItem> list) {
+        this.list = list;
+    }
+
+    public int addStock(StockItem item) {
+        if (item != null) {
+            // check if already have quantities of this item
+            StockItem inStock = list.getOrDefault(item.getName(), item);
+            // If there are already stocks on this item, adjust the quantity
+            if (inStock != item) {
+                item.adjustStock(inStock.getQuantityStock());
+            }
+            list.put(item.getName(), item);
+            return item.getQuantityStock();
+        }
+        return 0;
+    }
+
+    public int sellStock(String item, int quantity) {
+        StockItem inStock = list.getOrDefault(item, null);
+        if ((inStock != null) && (inStock.getQuantityStock() >= quantity) && (quantity > 0)) {
+            inStock.adjustStock(-quantity);
+            return quantity;
+        }
+        return 0;
+    }
+
+    public StockItem get(String key) {
+        return list.get(key);
+    }
+
+    public Map<String, StockItem> Items() {
+        return Collections.unmodifiableMap(list);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder("\nStock List\n");
+        double totalCost = 0.0;
+        for (Map.Entry<String, StockItem> item : list.entrySet()) {
+            StockItem stockItem = item.getValue();
+
+            double itemValue = stockItem.getPrice() * stockItem.getQuantityStock();
+
+            s.append(stockItem).append(". There are ").append(stockItem.getQuantityStock()).append(" in stock. Value of items: ");
+            s.append(itemValue).append("\n");
+            totalCost += itemValue;
+        }
+
+        return s + "Total stock value " + totalCost;
+    }
+}
